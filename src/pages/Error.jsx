d4dc8344475fae05aedebac5114093eb030f1959
giorgi <giorgikochuashvili2002@gmail.com>
2023-04-17 11:54:36 +0400

    finished error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,16 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found image' />
+        <h3>Page Not Found</h3>
+        <p>We Can't find the page you are looking for</p>
+        <Link to='/'>Back to Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;

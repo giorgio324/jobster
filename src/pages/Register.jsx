4a4ba218bb7fac3d5d57d2bf5b3eedfb47c5424f
giorgi <giorgikochuashvili2002@gmail.com>
2023-04-18 03:17:28 +0400

    added toasts,seperated inputs to custom components

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,71 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components/index';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { toast } from 'react-toastify';
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('Please fill all fields');
+    }
+  };
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+            labelText='name'
+          />
+        )}
+
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+          labelText='email'
+        />
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+          labelText='password'
+        />
+        <div className='form-row'></div>
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          {values.isMember ? 'Not a member ?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
